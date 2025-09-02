b9697993bb752721c495fcff48aa9cbfa525830a
Anatolij Andruscenko <andruscenko@intel471.com>
2025-09-03 01:05:25 +0200

    render multiple components

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,75 @@
 import './style.css'
-import { Effect } from "effect"
+import { Effect, Console } from "effect"
+
+
+
+const createVNode = (type: any, props: any, key: any, ref: any) => Effect.gen(function* () {
+  yield* Console.log(props)
+  const element = document.createElement(type)
+  for (let i in props) {
+    if (i === 'children') {
+      if (typeof props[i] === 'string') {
+        element.innerHTML = props[i]
+      } else {
+        element.appendChild(props[i])
+      }
+    }
+  }
+  return element;
+})
+
+// props example: { className: "test", id: "test", children: "test", ref: "test", key: "test" }
+const createElement = (
+  type: any,
+  props: Record<string, any> = {},
+  ...children: any[]
+) => Effect.gen(function* () {
+  let normalizedProps = {}
+  let key, ref;
+	for (let i in props) {
+		if (i == 'key') {
+      key = props[i];
+    }
+		else if (i == 'ref' && typeof type != 'function') {
+      ref = props[i];
+    }
+		else {
+      // @ts-ignore
+      normalizedProps[i] = props[i];
+    }
+	}
+
+	// 0 children: do nothing.
+	// 1 child: store it directly (no array).
+	// Many children: build an array only when needed.
+	if (children.length === 1) {
+    // @ts-ignore
+    normalizedProps.children = children[0];
+  } else if (children.length > 1) {
+    // @ts-ignore
+    normalizedProps.children = children;
+  }
+
+  // @ts-ignore
+	return yield* createVNode(type, normalizedProps, key, ref);
+})
+
 
 const render = (vnode: Effect.Effect<HTMLElement>, parentDom: HTMLElement) => Effect.gen(function* (){
   const result = yield* vnode
-  parentDom.appendChild(result)
+  parentDom.append(result)
+  yield* Console.log(result)
+})
+
+
+
+const Component2 = () => Effect.gen(function* () {
+  return yield* createElement("h1", {}, "Heading")
 })
 
 
 const Component1 = () => Effect.gen(function* () {
-  const h1 = document.createElement("h1")
-  h1.innerText = "Fist component ever"
-  return h1 as HTMLElement
+  return yield* createElement("div", {}, yield* Component2())
 })
 
 const root = document.getElementById('app') as HTMLElement;
