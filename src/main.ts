0df3a9f447de783a9155fd8f9c11acaf3bd381d8
ToljaFrajer <andruscenko@email.cz>
2025-09-03 19:54:27 +0200

    build vdom with effects

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,82 @@
-import './style.css'
-import { Effect, Console } from "effect"
+import { Effect, Console } from "effect";
 
+export interface VNode {
+  type: string | Component<any>;
+  props: Record<string, any>;
+  key?: any;
+  ref?: any;
+}
 
+export type Component<P = any> = (props: P) => Effect.Effect<VNode>;
 
-const createVNode = (type: any, props: any, key: any, ref: any) => Effect.gen(function* () {
-  yield* Console.log(props)
-  const element = document.createElement(type)
-  for (let i in props) {
-    if (i === 'children') {
-      if (typeof props[i] === 'string') {
-        element.innerHTML = props[i]
-      } else {
-        element.appendChild(props[i])
-      }
-    }
-  }
-  return element;
-})
+const createVNode = (
+  type: any,
+  props: Record<string, any>,
+  key?: any,
+  ref?: any
+) =>
+  Effect.succeed({
+    type,
+    props,
+    key,
+    ref,
+  });
 
-// props example: { className: "test", id: "test", children: "test", ref: "test", key: "test" }
-const createElement = (
+export const createElement = (
   type: any,
   props: Record<string, any> = {},
-  ...children: any[]
-) => Effect.gen(function* () {
-  let normalizedProps = {}
-  let key, ref;
-	for (let i in props) {
-		if (i == 'key') {
-      key = props[i];
+  ...children: Effect.Effect<any, never, never>[]
+) =>
+  Effect.gen(function* () {
+    let normalizedProps: any = {};
+    let key, ref;
+
+    for (let i in props) {
+      if (i === "key") key = props[i];
+      else if (i === "ref" && typeof type !== "function") ref = props[i];
+      else normalizedProps[i] = props[i];
     }
-		else if (i == 'ref' && typeof type != 'function') {
-      ref = props[i];
+
+    // Normalize children
+    if (children.length === 1) {
+      normalizedProps.children = yield* children[0];
+    } else if (children.length > 1) {
+      normalizedProps.children = yield* Effect.all(children);
+    } else {
+      normalizedProps.children = [];
     }
-		else {
-      // @ts-ignore
-      normalizedProps[i] = props[i];
+
+    // If type is a component, run it
+    if (typeof type === "function") {
+      return yield* type(normalizedProps) as Effect.Effect<VNode>;
     }
-	}
 
-	// 0 children: do nothing.
-	// 1 child: store it directly (no array).
-	// Many children: build an array only when needed.
-	if (children.length === 1) {
-    // @ts-ignore
-    normalizedProps.children = children[0];
-  } else if (children.length > 1) {
-    // @ts-ignore
-    normalizedProps.children = children;
-  }
+    return yield* createVNode(type, normalizedProps, key, ref);
+  });
 
-  // @ts-ignore
-	return yield* createVNode(type, normalizedProps, key, ref);
-})
-
-
-const render = (vnode: Effect.Effect<HTMLElement>, parentDom: HTMLElement) => Effect.gen(function* (){
-  const result = yield* vnode
-  parentDom.append(result)
-  yield* Console.log(result)
-})
+const render = (vnode: Effect.Effect<VNode>, parentDom: HTMLElement) => Effect.gen(function* () {
+  yield* Console.log(yield* vnode);
+});
 
 
 
-const Component2 = () => Effect.gen(function* () {
-  return yield* createElement("h1", {}, "Heading")
-})
+const Heading = () =>
+  Effect.gen(function* () {
+    yield* Console.log("Heading");
+    return yield* createElement("h1", {}, Effect.succeed("Hello World"));
+});
 
+const App = () =>
+  Effect.gen(function* () {
+    return yield*createElement("div", { id: "root" },
+      createElement(Heading),
+      createElement("p", {}, Effect.succeed("This is Effect-TS VDOM"))
+    );
+});
 
-const Component1 = () => Effect.gen(function* () {
-  return yield* createElement("div", {}, yield* Component2())
-})
+Effect.runPromise(
+  render(createElement(App, {}), document.getElementById("app")!)
+);
+
 
 const root = document.getElementById('app') as HTMLElement;
-
-
-Effect.runFork(render(Component1(), root))
